d569409d8f55f74167011c0c0b95653d6c29b28d
Wilfer Zuluaga <wdzuluaga54@misena.edu.co>
2024-08-08 15:17:49 -0500

    chore: Add cookie-parser middleware for parsing cookies

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import { createRoles } from './libs/initialSetup';
 import productsRoutes from './routes/product.routes';
 import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 createRoles(); // Create the roles in the database
@@ -15,6 +16,7 @@ app.set('pkg', pkg); // Save the package.json in the app
 
 app.use(morgan('dev')); // Log requests to the console
 app.use(express.json()); // Receive JSON data
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.json({
