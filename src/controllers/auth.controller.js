b7377ab8c55611b543885d8c654506237221311f
Wilfer Zuluaga <wdzuluaga54@misena.edu.co>
2024-08-08 14:25:48 -0500

    Not one conflict, today was a good day.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,4 @@
 import User from '../models/User';
-import jwt from 'jsonwebtoken';
-import config from '../config';
 import Role from '../models/Role';
 import { createAccessToken } from '../libs/jwt';
 
