bea96493a386ae91c453bfc62ded04eb1677177e
Wilfer Zuluaga <wdzuluaga54@misena.edu.co>
2024-08-07 21:36:10 -0500

    signin token handler

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,35 +1,47 @@
-import User from '../models/User';
-import jwt from 'jsonwebtoken';
-import config from '../config';
-import Role from '../models/Role';
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+import config from "../config";
+import Role from "../models/Role";
 
 export const signUp = async (req, res) => {
-    const {username, email, password, roles} = req.body;
+  const { username, email, password, roles } = req.body;
 
-    const newUser = new User({
-        username,
-        email,
-        password: await User.encryptPassword(password)
-    })
+  const newUser = new User({
+    username,
+    email,
+    password: await User.encryptPassword(password),
+  });
 
-    if (roles) {
-        const foundRoles = await Role.find({name: {$in: roles}})
-        newUser.roles = foundRoles.map(roles => roles._id)
-    } else {
-        const role = await Role.findOne({name: "user"})
-        newUser.roles = [role._id]
-    }
+  if (roles) {
+    const foundRoles = await Role.find({ name: { $in: roles } });
+    newUser.roles = foundRoles.map((roles) => roles._id);
+  } else {
+    const role = await Role.findOne({ name: "user" });
+    newUser.roles = [role._id];
+  }
 
-    const savedUser = await newUser.save();
-    console.log(savedUser);
+  const savedUser = await newUser.save();
+  console.log(savedUser);
 
-    const token = jwt.sign({id: savedUser._id}, config.SECRET, {
-        expiresIn: 86400 // 24 hours
-    })
+  const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
+    expiresIn: 86400, // 24 hours
+  });
 
-    res.status(200).json({token})
-}
+  res.status(200).json({ token });
+};
 
 export const signIn = async (req, res) => {
-    res.json('signin')
-}
+  const userFound = await User.findOne({ email: req.body.email }).populate("roles");
+
+  if (!userFound) return res.status(400).json({ message: "User not found" });
+
+  const matchPassword = await User.comparePassword(req.body.password, userFound.password);
+
+  if (!matchPassword) return res.status(401).json({ token: null, message: "Invalid password" });
+
+  const token = jwt.sign({id: userFound._id}, config.SECRET, {
+    expiresIn: 86400 // 24 hours
+  })
+
+  res.json({token})
+};
